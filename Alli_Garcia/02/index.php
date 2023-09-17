661bc1ae5a908cec5cc8dd3866660b55f1d8767b
Kori McClure <35781456+ganmcc6nku@users.noreply.github.com>
2023-09-17 02:48:09 -0400

    Add files via upload
    created the generateCard function, accepts an integer and an array. Will generate a card for the team member at the integers position in the array.

--- Alli_Garcia/02/index.php
+++ Alli_Garcia/02/index.php
@@ -6,6 +6,29 @@ function calculateAge($dateOfBirth) {
     $age = $currentDate->diff($dob)->y;
     return $age;
 }
+function generateCard($i,$team) {	
+                $age = calculateAge($team[$i]['birthday']);
+                echo '<header class="resume-header pt-4 pt-md-0">
+                     <div class="row">
+                         <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
+                             <img class="picture" src="' . $team[$i]['photo'] . '" alt="' . $team[$i]['name'] . '"><!--//to do:add source in php-->
+                         </div><!--//col-->
+                         <div class="col">
+                             <div class="row p-4 justify-content-center justify-content-md-between">
+                                 <div class="primary-info col-auto">
+                                     <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">' . $team[$i]['name'] . '</h1><!--//name line-->
+                                     <div class="title mb-3">' . $team[$i]['role'] . '</div><!--//role line-->
+                                     <div class="title mb-3">Age: ' . $age . ' years</div>
+                                     <a href=detail.php?index=' . $i . ' class="btn btn-secondary">' . $team[$i]['profile'] . '</a><!--//Link the index page to the detail page using a different GET value for each person-->
+                                 </div><!--//primary-info-->
+                                 <div class="secondary-info col-auto mt-2">' . $team[$i]['secondaryInfo'] . '</div><!--//secondary-info-->
+                             </div><!--//row-->
+                         </div><!--//col-->
+                     </div><!--//row-->
+                 </header>
+                 <br>';             
+}
+
 $team = [
     [
         'photo' => 'assets/images/profile0.jpg',
@@ -40,7 +63,6 @@ $team = [
         'birthday' => '01/14/2001'
     ],
 ];
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -73,60 +95,14 @@ $team = [
     <article class="resume-wrapper text-center position-relative">
         <div class="resume-wrapper-inner mx-auto text-start bg-white shadow-lg">
             <h1 class="py-4 text-center">OUR AMAZING TEAM</h1>
-
-            <?php for ($i = 0; $i < count($team); $i++) {
-                $age = calculateAge($team[$i]['birthday']);
-                echo '<header class="resume-header pt-4 pt-md-0">
-                     <div class="row">
-                         <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-                             <img class="picture" src="' . $team[$i]['photo'] . '" alt="' . $team[$i]['name'] . '"><!--//to do:add source in php-->
-                         </div><!--//col-->
-                         <div class="col">
-                             <div class="row p-4 justify-content-center justify-content-md-between">
-                                 <div class="primary-info col-auto">
-                                     <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">' . $team[$i]['name'] . '</h1><!--//name line-->
-                                     <div class="title mb-3">' . $team[$i]['role'] . '</div><!--//role line-->
-                                     <div class="title mb-3">Age: ' . $age . ' years</div>
-                                     <a href=detail.php?index=' . $i . ' class="btn btn-secondary">' . $team[$i]['profile'] . '</a><!--//Link the index page to the detail page using a different GET value for each person-->
-                                 </div><!--//primary-info-->
-                                 <div class="secondary-info col-auto mt-2">' . $team[$i]['secondaryInfo'] . '</div><!--//secondary-info-->
-                             </div><!--//row-->
-                         </div><!--//col-->
-                     </div><!--//row-->
-                 </header>
-                 <br>';
-            } ?>
-
-            <!--original element code
-            <header class="resume-header mt-4 pt-4 pt-md-0">
-                <div class="row">
-                    <div class="col-block col-md-auto resume-picture-holder text-center text-md-start">
-                        <img class="picture" src="assets/images/profile.jpg" alt="">
-                    </div>
-                    <div class="col">
-                        <div class="row p-4 justify-content-center justify-content-md-between">
-                            <div class="primary-info col-auto">
-                                <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Member 2 name</h1>
-                                <div class="title mb-3">Member 2 role</div>
-                                <a href="# /*LINK_TO_MEMBER_2_PAGE*/ " class="btn btn-secondary">See full profile</a>
-                            </div>
-                            <div class="secondary-info col-auto mt-2">Secondary info
-                            </div>
-                        </div>
-                        
-                    </div>
-                </div>
-            </header>-->
+				<?php for($i = 0; $i < count($team); $i++){generateCard($i,$team);};?><!--iterates through team and displays a card for each member.
         </div>
     </article>
-
 
     <footer class="footer text-center pt-2 pb-5">
         <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
         <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart"></i> by Kori, Alli, Cody, and Dillon</small>
     </footer>
-
-
 
 </body>
 
